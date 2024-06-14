0cb70eba9dc85f9d99f097a60eda68284a8a4e8f
EYoung21 <eliyoung4now@email.com>
2024-06-14 07:00:41 -0400

    added stopwatch to run in background

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -7,12 +7,12 @@ import {
   Animated,
   SafeAreaView,
 } from 'react-native';
-import moment from 'moment';
+import { Stopwatch } from 'react-native-stopwatch-timer';
 import styles from './HomePageStyles';
 import { CollapseContext } from './FullApp';
 import SystemNavigationBar from 'react-native-system-navigation-bar';
 import { TotalElapsedContext } from './TotalElapsedContext';
-
+import moment from 'moment';
 
 const getTimerColor = (backgroundPalette: string) => {
   if (backgroundPalette === '#000000') {
@@ -21,19 +21,7 @@ const getTimerColor = (backgroundPalette: string) => {
   return '#0d0d0d';
 };
 
-function Timer({ interval, color }: { interval: number, color: string }) {
-  const duration = moment.duration(interval);
-  const centiseconds = Math.floor(duration.milliseconds() / 10).toString().padStart(2, '0');
-  const seconds = duration.seconds().toString().padStart(2, '0');
-  const minutes = duration.minutes().toString().padStart(2, '0');
-  const hours = Math.floor(duration.asHours()).toString().padStart(2, '0');
-
-  return (
-    <Text style={[styles.timer, { color }]}>
-      {hours}:{minutes}:{seconds}.{centiseconds}
-    </Text>
-  );
-}
+type SafeImageKey = 'image1' | 'image2' | 'image3';
 
 const HomeScreen = () => {
   const scaleAnim = useRef(new Animated.Value(1)).current;
@@ -43,27 +31,26 @@ const HomeScreen = () => {
 
   const { collapsed, setCollapsed } = useContext(CollapseContext);
   const { totalElapsedTime, setTotalElapsedTime, totalCurrency, setTotalCurrency, backgroundColor, setBackgroundColor, buttonColor, setButtonColor, buttonBorder, setButtonBorder, safe, setSafe } = useContext(TotalElapsedContext);
-  const [timerInterval, setTimerInterval] = useState(0);
+  const [stopwatchStart, setStopwatchStart] = useState(false);
+  const [stopwatchReset, setStopwatchReset] = useState(false);
   const [lockInTime, setLockInTime] = useState(0);
   const [elapsedTime, setElapsedTime] = useState(0);
   const [isActionInProgress, setIsActionInProgress] = useState(false);
 
-  useEffect(() => {
-    let timer: any;
-    if (isLockedIn) {
-      timer = setInterval(() => {
-        setTimerInterval((prevInterval) => prevInterval + 10);
-      }, 10);
-    } else {
-      clearInterval(timer);
-      setTimerInterval(0);
-    }
-    return () => clearInterval(timer);
-  }, [isLockedIn]);
+  // setTotalCurrency(500000);
+  // setTotalElapsedTime(500000);
 
   useEffect(() => {
     setImageSource(getSafeImage(safe, 'image2'));
   }, [safe]);
+
+  useEffect(() => {
+    if (!isLockedIn) {
+      const elapsedTimeInMinutes = elapsedTime / (1000 * 60);
+      setTotalElapsedTime((prevTime: number) => prevTime + elapsedTimeInMinutes);
+      setTotalCurrency((prevCurrency: number) => prevCurrency + elapsedTimeInMinutes);
+    }
+  }, [elapsedTime]);
 
   const handlePressIn = () => {
     if (isActionInProgress) return;
@@ -81,9 +68,9 @@ const HomeScreen = () => {
     { id: 'LargeSafe', image1: require('./assets/safe41.png'), image2: require('./assets/safe42.png'), image3: require('./assets/safe43.png'), cost: 1920 },
     { id: 'TreasureChest', image1: require('./assets/safe51.png'), image2: require('./assets/safe52.png'), image3: require('./assets/safe53.png'), cost: 2400 },
   ];
-
-  const getSafeImage = (id:string, imageKey:string) => {
-    const safe = safes.find(safe => safe.id === id);
+  
+  const getSafeImage = (id: string, imageKey: SafeImageKey) => {
+    const safe = safes.find((safe) => safe.id === id);
     return safe ? safe[imageKey] : null;
   };
 
@@ -94,20 +81,19 @@ const HomeScreen = () => {
     }).start();
 
     if (isLockedIn) {
+      const currentTime = Date.now();
       setIsLockedIn(false);
       setCollapsed(false);
       SystemNavigationBar.navigationShow();
       setImageSource(getSafeImage(safe, 'image1'));
       setButtonText('Lock In');
-      setElapsedTime(Date.now() - lockInTime);
+      setElapsedTime(currentTime - lockInTime);
       setLockInTime(0);
+      setStopwatchStart(false);
+      setStopwatchReset(true);
       setTimeout(() => {
         setImageSource(getSafeImage(safe, 'image2'));
       }, 625);
-
-      const elapsedTimeInMinutes = elapsedTime / (1000 * 60);
-      setTotalElapsedTime(totalElapsedTime + elapsedTimeInMinutes);
-      setTotalCurrency(totalCurrency + elapsedTimeInMinutes);
     } else {
       setIsLockedIn(true);
       setCollapsed(true);
@@ -115,6 +101,8 @@ const HomeScreen = () => {
       setImageSource(getSafeImage(safe, 'image3'));
       setButtonText('Lock Out');
       setLockInTime(Date.now());
+      setStopwatchStart(true);
+      setStopwatchReset(false);
     }
     setTimeout(() => {
       setIsActionInProgress(false);
@@ -124,44 +112,31 @@ const HomeScreen = () => {
   const getImageStyle = (source: any) => {
     if (source === require('./assets/safe1.png')) {
       return styles.image2;
-    }
-    else if (source === require('./assets/safe2.png')) {
+    } else if (source === require('./assets/safe2.png')) {
       return styles.image5;
-    }
-    else if (source === require('./assets/safe3.png')) {
+    } else if (source === require('./assets/safe3.png')) {
       return styles.image;
-    }
-    else if (source === require('./assets/safe21.png')) {
+    } else if (source === require('./assets/safe21.png')) {
       return styles.image3;
-    } 
-    else if (source === require('./assets/safe22.png')) {
+    } else if (source === require('./assets/safe22.png')) {
       return styles.image4;
-    }
-    else if (source === require('./assets/safe23.png')) {
+    } else if (source === require('./assets/safe23.png')) {
       return styles.image14;
-    }
-    else if (source === require('./assets/safe33.png')) {
+    } else if (source === require('./assets/safe33.png')) {
       return styles.image6;
-    }
-    else if (source === require('./assets/safe31.png') || source === require('./assets/safe32.png')) {
+    } else if (source === require('./assets/safe31.png') || source === require('./assets/safe32.png')) {
       return styles.image7;
-    }
-    else if (source === require('./assets/safe43.png')) {
+    } else if (source === require('./assets/safe43.png')) {
       return styles.image8;
-    }
-    else if (source === require('./assets/safe41.png')) {
+    } else if (source === require('./assets/safe41.png')) {
       return styles.image9;
-    }
-    else if (source === require('./assets/safe42.png')) {
+    } else if (source === require('./assets/safe42.png')) {
       return styles.image10;
-    }
-    else if (source === require('./assets/safe53.png')) {
+    } else if (source === require('./assets/safe53.png')) {
       return styles.image11;
-    }
-    else if (source === require('./assets/safe51.png')) {
+    } else if (source === require('./assets/safe51.png')) {
       return styles.image12;
-    }
-    else if (source === require('./assets/safe52.png')) {
+    } else if (source === require('./assets/safe52.png')) {
       return styles.image13;
     }
   };
@@ -169,22 +144,45 @@ const HomeScreen = () => {
   return (
     <SafeAreaView style={[styles.container, { backgroundColor }]}>
       <Text style={[styles.heading, styles.totalCurrency]}>$ in minutes: {totalCurrency.toFixed(2)}</Text>
-      <Timer interval={timerInterval} color={getTimerColor(backgroundColor)} />
+      <Stopwatch
+        laps
+        msecs
+        start={stopwatchStart}
+        reset={stopwatchReset}
+        options={options}
+        getTime={(time) => setElapsedTime(moment.duration(time).asMilliseconds())}
+      />
       <Image source={imageSource} style={getImageStyle(imageSource)} />
       <TouchableWithoutFeedback onPressIn={handlePressIn} onPressOut={handlePressOut}>
-        <Animated.View style={[
-          styles.button, 
-          { 
-            transform: [{ scale: scaleAnim }], 
-            backgroundColor: buttonColor, 
-            borderColor: buttonBorder,
-          }
-        ]}>
+        <Animated.View
+          style={[
+            styles.button,
+            {
+              transform: [{ scale: scaleAnim }],
+              backgroundColor: buttonColor,
+              borderColor: buttonBorder,
+            },
+          ]}
+        >
           <Text style={styles.buttonText}>{buttonText}</Text>
         </Animated.View>
       </TouchableWithoutFeedback>
     </SafeAreaView>
   );
+};
+
+const options = {
+  container: {
+    backgroundColor: '#000',
+    padding: 5,
+    borderRadius: 5,
+    width: 220,
+  },
+  text: {
+    fontSize: 30,
+    color: '#FFF',
+    marginLeft: 7,
+  },
 };
 
 export default HomeScreen;
