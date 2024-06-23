d838c79d6bebd03b7b30e642445a98e43aa8fe47
EYoung21 <eliyoung4now@email.com>
2024-06-23 05:39:26 -0400

    RN sign in and metro error

---
+++ declarations.d.ts
@@ -0,0 +1,5 @@
+declare module '@env' {
+    export const GOOGLE_WEB_CLIENT_ID: string;
+    export const SUPABASEURL: string;
+    export const SUPABASEKEY: string;
+  }
--- google-sheets-backend/index.js
+++ google-sheets-backend/index.js
@@ -3,26 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { google } = require('googleapis');
 const { createClient } = require('@supabase/supabase-js');
+
 const credentials = {
-    type: 'service_account',
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URI,
-    token_uri: process.env.TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-    universe_domain: process.env.UNIVERSE_DOMAIN
-  };
+  type: 'service_account',
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+};
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-// Initialize the JWT client using the service account credentials
 const jwtClient = new google.auth.JWT(
   credentials.client_email,
   null,
@@ -33,8 +33,8 @@ const jwtClient = new google.auth.JWT(
 const drive = google.drive({ version: 'v3', auth: jwtClient });
 const sheets = google.sheets({ version: 'v4', auth: jwtClient });
 
-const supabaseUrl = 'LockIn/supabaseClient.js';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2YWpqaGpldXdxemdrdXp4bmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5NjU2OTYsImV4cCI6MjAzNDU0MTY5Nn0.wYqQ4qrNL-cKPDnsIznBuCCnHwXljU-oNzd5wyXCo_U';
+const supabaseUrl = process.env.SUPABASEURL;
+const supabaseAnonKey = process.env.SUPABASEKEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 app.post('/storeUser', async (req, res) => {
@@ -46,7 +46,6 @@ app.post('/storeUser', async (req, res) => {
       throw error;
     }
 
-    // Store user data in Supabase
     const { id, email, ...userData } = data;
     const { error: dbError } = await supabase
       .from('users')
@@ -64,10 +63,8 @@ app.post('/storeUser', async (req, res) => {
 
 app.post('/createSheet', async (req, res) => {
   try {
-    // Authorize the client
     await jwtClient.authorize();
 
-    // Create a new Google Sheet
     const response = await drive.files.create({
       requestBody: {
         name: 'LockInData',
@@ -77,8 +74,7 @@ app.post('/createSheet', async (req, res) => {
 
     const fileId = response.data.id;
 
-    // Add data to the Google Sheet
-    const { data } = req.body;  // Expecting an array of arrays from the client
+    const { data } = req.body;
     await sheets.spreadsheets.values.update({
       spreadsheetId: fileId,
       range: 'Sheet1!A1',
@@ -86,7 +82,7 @@ app.post('/createSheet', async (req, res) => {
       resource: {
         values: [
           ['Week', 'Total Hours', 'Total Minutes', 'Avg Hours', 'Avg Minutes', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-          ...data,  // Spread the data rows into the values array
+          ...data,
         ],
       },
     });
