e93884f7c538afb71784ade70a33a26698a521e6
EYoung21 <eliyoung4now@email.com>
2024-06-29 17:10:33 -0400

    configured notifications

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ if (typeof process.cwd === 'undefined') {
 }
 
 import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import FullApp from './FullApp';
 import { name as appName } from './app.json';
 import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 if (typeof process !== 'undefined' && typeof process.cwd === 'function') {
   require('dotenv').config();
@@ -20,9 +21,24 @@ if (typeof process !== 'undefined' && typeof process.cwd === 'function') {
 PushNotification.configure({
   onNotification: function (notification) {
     console.log('NOTIFICATION:', notification);
+    notification.finish(PushNotificationIOS.FetchResult.NoData); // Add this line to handle iOS notifications
   },
   popInitialNotification: true,
   requestPermissions: Platform.OS === 'ios',
 });
+
+// Create a channel (required for Android)
+PushNotification.createChannel(
+  {
+    channelId: 'your-channel-id',
+    channelName: 'My channel',
+    channelDescription: 'A channel to categorize your notifications',
+    playSound: true,
+    soundName: 'default',
+    importance: 4, // Importance.HIGH (4)
+    vibrate: true,
+  },
+  (created) => console.log(`createChannel returned '${created}'`)
+);
 
 AppRegistry.registerComponent(appName, () => FullApp);
--- ios/LockIn/AppDelegate.mm
+++ ios/LockIn/AppDelegate.mm
@@ -2,63 +2,20 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import <ReactNativeReanimated/ReactNativeReanimated.h>
-
-// Add Google Sign-In imports
-#import <GoogleSignIn/GoogleSignIn.h>
-
-// Add Push Notification imports
-#import <UserNotifications/UserNotifications.h>
+#import <UserNotifications/UNUserNotificationCenter.h>
 #import <RNCPushNotificationIOS.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"LockIn"
-                                            initialProperties:nil];
-
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-
-  // Initialize Google Sign-In
-  [GIDSignIn sharedInstance].clientID = @"YOUR_IOS_CLIENT_ID";
-
-  // Register for Push Notifications
+  // Add this line to register for push notifications
   UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
   center.delegate = self;
-  UNAuthorizationOptions options = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
-  [center requestAuthorizationWithOptions:options
-                        completionHandler:^(BOOL granted, NSError * _Nullable error) {
-    if (!error) {
-      dispatch_async(dispatch_get_main_queue(), ^{
-        [[UIApplication sharedApplication] registerForRemoteNotifications];
-      });
-    }
-  }];
   
+  // Other existing code...
+
   return YES;
-}
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
-
-// Add this method to handle the URL that the application receives at the end of the Google Sign-In process
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options {
-  return [[GIDSignIn sharedInstance] handleURL:url];
 }
 
 // Required for the register event.
@@ -80,20 +37,13 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
   [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
 }
 
-// Required for the localNotification event.
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
-{
-  [RNCPushNotificationIOS didReceiveLocalNotification:notification];
-}
-
-// Handle notification responses
+// Required for local notifications
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
        willPresentNotification:(UNNotification *)notification
-         withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
 {
-  NSDictionary *userInfo = notification.request.content.userInfo;
-  [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo];
-  completionHandler(UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionBadge);
+  [RNCPushNotificationIOS didReceiveNotification:notification];
+  completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
 }
 
 @end
