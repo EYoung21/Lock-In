db7c59934db08d6a4b96651582080a7b37850965
EYoung21 <eliyoung4now@gmail.com>
2024-08-13 03:36:14 -0400

    comments

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin',
+    'react-native-reanimated/plugin',  // Ensure this is included for React Native Reanimated
     ['module:react-native-dotenv', {
       moduleName: '@env',
       path: '.env',
     }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],  // Add this line
+    ['@babel/plugin-transform-private-methods', { loose: true }]  // Added line for private methods
   ],
 };
