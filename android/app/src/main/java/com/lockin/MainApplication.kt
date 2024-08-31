5920e017f5aba406b82c49610a7ee39afb59bd82
EYoung21 <eliyoung4now@gmail.com>
2024-08-31 13:12:03 -0400

    scrapped kotlin app monitoring completely this time

--- android/app/src/main/java/com/lockin/MainApplication.kt
+++ android/app/src/main/java/com/lockin/MainApplication.kt
@@ -6,15 +6,14 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.lockin.modules.CurrentAppPackage // Import your package
 import com.facebook.react.PackageList // Import PackageList
 
 class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> {
-            // Add your custom package here along with the packages from PackageList
-            return PackageList(this).packages + listOf(CurrentAppPackage())
+            // Return packages from PackageList only
+            return PackageList(this).packages
         }
 
         override fun getJSMainModuleName(): String = "index"
